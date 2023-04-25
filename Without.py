65a2f2147dd2dabde9fdc3730632ee123e101b01
Keisuke54 <keinakamura5420@gmail.com>
2023-04-25 11:08:06 +0800

    Error fixed

--- Without.py
+++ Without.py
@@ -16,14 +16,15 @@ image = np.asarray(image)
 
 predict = model.predict(image, conf=0.25)
 results = predict[0].boxes.data
+print(results)
 
 for result in results:
     x1 = int(result[0])
     y1 = int(result[1])
     x2 = int(result[2])
     y2 = int(result[3])
-    label = result[4]
-    confidence = result[5]
+    confidence = result[4]
+    label = result[5]
     cv2.rectangle(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
     text = f"{label}: {confidence:.2f}"
     cv2.putText(image, text, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
