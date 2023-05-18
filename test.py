8ef57ae07def9f7e837dad97fc70b66d14ec58ab
Keisuke54 <keinakamura5420@gmail.com>
2023-05-19 00:26:06 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,1 @@
 
-exList = """1. What is a normal force?
-2. What is a normal force used for?
-3. What is a normal force in physics?
-4. What is a normal force in mechanics?
-5. What is a normal force in mathematics?
-6. What is a normal force in computer science?
-7. What is a normal force in engineering?
-8. What is a normal force in biology?
-9. What is a normal force in chemistry?
-10. What is a normal force in physics?"""
-
-exex = exList.replace('\n', ', ')
-
-exexList = list(exex.split(', '))
-
-print(exexList)
