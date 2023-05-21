98fb9a8e02329c776ee9f00e6ad575f18612f748
Keisuke54 <keinakamura5420@gmail.com>
2023-05-21 08:47:06 +0800

    update

--- test.py
+++ test.py
@@ -3,36 +3,38 @@ import tensorflow as tf
 
 from keras.models import load_model
 
-from conversation import conversation
+from conversation2 import conversation
 
+with tf.device('/cpu:0'):
+    Tokenizer = tf.keras.preprocessing.text.Tokenizer
+    
+    pad_sequences = tf.keras.preprocessing.sequence.pad_sequences
+    
+    tokenizer = Tokenizer(char_level=True, lower=True)
+    tokenizer.fit_on_texts(conversation)
+    
+    def generate_text(seed_text, model, tokenizer, sequence_length, num_chars_to_generate):
+        generated_text = seed_text
+        
+        for _ in range(num_chars_to_generate):
+            token_list = tokenizer.texts_to_sequences([generated_text])
+            token_list = pad_sequences(token_list, maxlen=sequence_length, padding="pre")
+            predicted_probs = model.predict(token_list, verbose=0)
+            predicted_token = np.argmax(predicted_probs, axis=-1)[0]  
+            
+            output_word = ""
+            for word, index in tokenizer.word_index.items():
+                if index == predicted_token:
+                    output_word = word
+                    break
+            
+            generated_text += output_word
 
-Tokenizer = tf.keras.preprocessing.text.Tokenizer
-
-pad_sequences = tf.keras.preprocessing.sequence.pad_sequences
-
-tokenizer = Tokenizer(char_level=True, lower=True)
-tokenizer.fit_on_texts(conversation)
-
-# evaluation and generating sample text 
-def generate_text(seed_text, model, tokenizer, sequence_length, num_chars_to_generate):
-    generated_text = ''
-    for _ in range(num_chars_to_generate):
-        token_list = tokenizer.texts_to_sequences([seed_text])
-        token_list = pad_sequences(token_list, maxlen=sequence_length, padding="pre")
-        predicted_probs = model.predict(token_list, verbose=0)
-        predicted_token = np.argmax(predicted_probs, axis=-1)[0]  
-
-        output_word = ""
-        for word, index in tokenizer.word_index.items():
-            if index == predicted_token:
-                output_word = word
-                break
-        seed_text = seed_text + output_word
-        generated_text += output_word
-
-    return generated_text
-
-modelF = load_model('model.h5')
-seed_text = "Question: How can I find electric potential at a point?"
-generated_text = generate_text(seed_text, modelF, tokenizer, 80, num_chars_to_generate=300)
-print(generated_text)
+        return generated_text
+    
+    modelF = load_model('model.h5', compile=False)
+    modelF.compile(loss="sparse_categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+    seed_text = "Question: How can I calculate torque?"
+    
+    generated_text = generate_text(seed_text, modelF, tokenizer, 100, num_chars_to_generate=300)
+    print(generated_text)
